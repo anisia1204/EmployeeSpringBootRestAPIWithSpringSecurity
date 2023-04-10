6275b17e90673407c36bcb4a0421bdd5bee20e08
anisia1204 <anisia.rosu@student.upt.ro>
2023-04-10 12:38:17 +0300

    added 3 users in the config class

--- 00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -1,8 +1,37 @@
 package com.luv2code.springboot.cruddemo.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    //since we defined our users here, Spring Boot will not use the user/pass from
+    //the application.properties file
+    //it will use this User Details Manager from this config
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() { //I'm making use of the in memory authentication
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build(); //builder design pattern
+
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
 }
